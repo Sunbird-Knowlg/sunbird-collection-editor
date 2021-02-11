051bfeec48c54e540cdc280aaed81fba0304df88
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-02-11 20:07:07 +0530

    #DP-1547 feat: added add from library button  enable/disable based on config

--- projects/collection-editor-library/src/lib/components/fancy-tree/fancy-tree.component.ts
+++ projects/collection-editor-library/src/lib/components/fancy-tree/fancy-tree.component.ts
@@ -23,6 +23,7 @@ export class FancyTreeComponent implements OnInit, AfterViewInit, OnDestroy {
   public config: any;
   public showTree: boolean;
   public rootNode: any;
+  public showLibraryButton =  false;
   public rootMenuTemplate = `<span class="ui dropdown sb-dotted-dropdown" autoclose="itemClick" suidropdown="" tabindex="0">
   <span id="contextMenu" class="p-0 w-auto"><i class="icon ellipsis vertical sb-color-black"></i></span>
   <span id= "contextMenuDropDown" class="menu transition hidden" suidropdownmenu="" style="">
@@ -115,6 +116,7 @@ export class FancyTreeComponent implements OnInit, AfterViewInit, OnDestroy {
       if (rootNode) {
         this.treeService.setActiveNode(rootNode);
       }
+      this.addFromLibraryButton(0);
     });
     this.showTree = true;
   }
@@ -197,6 +199,7 @@ export class FancyTreeComponent implements OnInit, AfterViewInit, OnDestroy {
       click: (event, data): boolean => {
         this.tree.nativeElement.click();
         const node = data.node;
+        this.addFromLibraryButton(node.getLevel() - 1);
         // this.treeEventEmitter.emit({ 'type': 'nodeSelect', 'data': node });
         this.telemetryService.interact({ edata: this.getTelemetryInteractEdata()});
         return true;
@@ -436,6 +439,14 @@ export class FancyTreeComponent implements OnInit, AfterViewInit, OnDestroy {
         break;
     }
   }
+  addFromLibraryButton(nodeLevel) {
+    if (nodeLevel === 0) {
+     _.isEmpty(_.get(this.config, 'children')) ? this.showLibraryButton = false : this.showLibraryButton = true;
+    } else {
+     const hierarchylevelData =  this.config.hierarchy[`level${nodeLevel}`];
+     _.isEmpty(_.get(hierarchylevelData, 'children')) ? this.showLibraryButton = false : this.showLibraryButton = true;
+    }
+   }
   addFromLibrary() {
     this.editorService.emitshowLibraryPageEvent('showLibraryPage');
   }
