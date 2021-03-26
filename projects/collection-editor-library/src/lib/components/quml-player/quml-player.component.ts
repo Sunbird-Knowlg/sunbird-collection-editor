00f8771a6210a81c54c7c9222b5fe435692cdefb
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2021-03-26 14:33:27 +0530

    Issue #SB-23174 fix: removed formConfig service

--- projects/collection-editor-library/src/lib/components/quml-player/quml-player.component.ts
+++ projects/collection-editor-library/src/lib/components/quml-player/quml-player.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as _ from 'lodash-es';
-import { ServerResponse } from '../../interfaces/serverResponse';
-import { EditorService } from '../../services/editor/editor.service';
-import { FormService } from '../../services/form/form.service';
+import { ConfigService } from '../../services/config/config.service';
 import { PlayerService } from '../../services/player/player.service';
 
 @Component({
@@ -15,41 +13,22 @@ export class QumlPlayerComponent implements OnInit {
   @Input() questionSetHierarchy: any;
   @Input() questionIds: any;
   showPreview = false;
-  constructor(private editorService: EditorService, private formService: FormService,
-              private playerService: PlayerService ) { }
+  constructor(private configService: ConfigService, private playerService: PlayerService ) { }
 
   ngOnInit() {
     this.initialize();
   }
 
   initialize() {
-    const formReadInputParams = {
-      formType: 'content',
-      formAction: 'play',
-      contentType: 'player',
-      rootOrgId: _.get(this.editorService, 'editorConfig.context.channel')
-    };
-    this.formService.getFormConfig(formReadInputParams).subscribe(
-      (data: any) => {
-       this.setQumlPlayerData(data);
-       this.showPreview = true;
-      }, (err: ServerResponse) => {
-        const errInfo = {
-          errorMsg: 'Fetching player detailes failed. Try again later',
-        };
-        this.editorService.apiErrorHandling(err, errInfo);
-    });
+    this.setQumlPlayerData();
+    this.showPreview = true;
   }
 
-  setQumlPlayerData(formConfigData) {
+  setQumlPlayerData() {
     const playerConfig = _.cloneDeep(this.playerService.getQumlPlayerConfig());
     this.qumlPlayerConfig = playerConfig;
     this.qumlPlayerConfig.metadata = _.cloneDeep(this.questionSetHierarchy);
-    _.forEach(formConfigData, (value) => {
-      if (_.includes(_.get(value, 'mimeType'), this.qumlPlayerConfig.metadata.mimeType) && _.get(value, 'version') === 2) {
-        this.qumlPlayerConfig.metadata.threshold = _.get(value, 'threshold');
-      }
-    });
+    this.qumlPlayerConfig.metadata.threshold = _.get(this.configService, 'playerConfig.threshold');
     this.qumlPlayerConfig.metadata.totalQuestions = 1;
     // tslint:disable-next-line:max-line-length
     this.qumlPlayerConfig.metadata.maxQuestions = this.qumlPlayerConfig.metadata.maxQuestions || this.qumlPlayerConfig.metadata.totalQuestions;
