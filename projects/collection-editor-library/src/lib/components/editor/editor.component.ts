52effa930a7a3867343e6ceb980503b807ed8afa
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-05-06 17:25:30 +0530

    Issue #SB-24462 fix: The timer shows incorrect values on the preview pop-up for the Question set

--- projects/collection-editor-library/src/lib/components/editor/editor.component.ts
+++ projects/collection-editor-library/src/lib/components/editor/editor.component.ts
@@ -424,6 +424,9 @@ export class EditorComponent implements OnInit, OnDestroy, AfterViewInit {
   }
   updateTreeNodeData() {
       const treeNodeData =  _.get(this.treeService.getFirstChild(), 'data.metadata');
+      if (!treeNodeData.timeLimits) {
+        treeNodeData.timeLimits = {};
+      }
       if (treeNodeData.maxTime) {
         treeNodeData.timeLimits.maxTime = this.helperService.hmsToSeconds(treeNodeData.maxTime);
       }
