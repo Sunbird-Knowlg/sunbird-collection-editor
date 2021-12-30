e1ae609dad785c91b40b2a90e66a4c42e4536c7f
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-12-31 02:03:23 +0530

    Creator added in question create

--- projects/collection-editor-library/src/lib/components/question/question.component.ts
+++ projects/collection-editor-library/src/lib/components/question/question.component.ts
@@ -631,7 +631,7 @@ export class QuestionComponent implements OnInit, AfterViewInit, OnDestroy {
   getDefaultSessionContext() {
     return _.omitBy(_.merge(
       {
-        author: _.get(this.editorService.editorConfig, 'context.user.fullName'),
+        creator: _.get(this.editorService.editorConfig, 'context.user.fullName'),
         createdBy: _.get(this.editorService.editorConfig, 'context.user.id'),
         ..._.pick(_.get(this.editorService.editorConfig, 'context'), ['board', 'medium', 'gradeLevel', 'subject', 'topic'])
       },
