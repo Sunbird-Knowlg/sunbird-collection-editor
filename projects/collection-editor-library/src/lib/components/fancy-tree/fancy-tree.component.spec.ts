829b90a7636f06cdea0c91ff28306a14e88ea898
pradeepkumarcm96 <pradeep.k@pacewisdom.com>
2022-01-24 17:16:05 +0530

    Issue #survey-test : test : test case fix

--- projects/collection-editor-library/src/lib/components/fancy-tree/fancy-tree.component.spec.ts
+++ projects/collection-editor-library/src/lib/components/fancy-tree/fancy-tree.component.spec.ts
@@ -14,7 +14,7 @@ import { ConfigService } from '../../services/config/config.service';
 import { SuiModule } from 'ng2-semantic-ui-v9';
 import { HelperService } from 'collection-editor-library/lib/services/helper/helper.service';
 import { BranchingLogic } from '../question/question.component.spec.data';
-fdescribe('FancyTreeComponent', () => {
+describe('FancyTreeComponent', () => {
   let component: FancyTreeComponent;
   let fixture: ComponentFixture<FancyTreeComponent>;
   let editorService,helperService;
