59e2e710481e9caf9fb525ea135d3fcf1bde2583
Rajnish Dargan <rajnish.code30@gmail.com>
2022-03-08 14:22:17 +0530

    Issue #SB-28566 feat: Enable reuse of questions from question set editor

--- projects/collection-editor-library/src/lib/components/fancy-tree/fancy-tree.component.ts
+++ projects/collection-editor-library/src/lib/components/fancy-tree/fancy-tree.component.ts
@@ -287,17 +287,15 @@ export class FancyTreeComponent implements OnInit, AfterViewInit, OnDestroy {
       this.visibility.addSibling = ((node.folder === true) && (!node.data.root) && !((node.getLevel() - 1) > this.config.maxDepth)) ? true : false;
       if (nodeLevel === 0) {
         this.visibility.addFromLibrary = _.isEmpty(_.get(this.config, 'children')) || _.get(this.config, 'enableQuestionCreation') === false ? false : true;
-        this.visibility.addQuestionFromLibrary = ((_.get(this.config, 'enableAddFromLibrary') === false ? false : true) &&
-        !_.isEmpty(_.get(this.config, 'children')));
         this.visibility.createNew = _.isEmpty(_.get(this.config, 'children')) || _.get(this.config, 'enableQuestionCreation') === false ? false : true;
+        this.visibility.addQuestionFromLibrary = _.isEmpty(_.get(this.config, 'children')) || _.get(this.config, 'enableAddFromLibrary') === false ? false : true;
       } else {
         const hierarchylevelData = this.config.hierarchy[`level${nodeLevel}`];
         // tslint:disable-next-line:max-line-length
         this.visibility.addFromLibrary = ((node.folder === false) || _.isEmpty(_.get(hierarchylevelData, 'children')) || _.get(this.config, 'enableQuestionCreation') === false) ? false : true;
-        this.visibility.addQuestionFromLibrary = ((node.folder === true) && (_.get(hierarchylevelData, 'enableAddFromLibrary') === false ?
-        false : true) && !_.isEmpty(_.get(hierarchylevelData, 'children')));
         // tslint:disable-next-line:max-line-length
         this.visibility.createNew = ((node.folder === false) || _.isEmpty(_.get(hierarchylevelData, 'children')) || _.get(this.config, 'enableQuestionCreation') === false) ? false : true;
+        this.visibility.addQuestionFromLibrary = ((node.folder === false) || _.isEmpty(_.get(hierarchylevelData, 'children')) || _.get(this.config, 'enableAddFromLibrary') === false) ? false : true;
       }
     }
     this.cdr.detectChanges();
