3be7ebb6fd11cefa35094fb910b1e455852e0a23
pradeepkumarcm96 <pradeep.k@pacewisdom.com>
2022-05-30 11:26:20 +0530

    Issue #SB-21729 : fix : unused code removed

--- projects/collection-editor-library/src/lib/components/question/question.component.ts
+++ projects/collection-editor-library/src/lib/components/question/question.component.ts
@@ -1200,10 +1200,7 @@ export class QuestionComponent implements OnInit, AfterViewInit, OnDestroy {
             if (formFieldCategory.code === 'markAsNotMandatory') {
               defaultValue === 'Yes' ? (defaultValue = 'No') : (defaultValue = 'Yes');
             }
-            if (formFieldCategory.code === 'maxScore' && this.questionInteractionType === 'choice') {
-              defaultValue = this.maxScore;
-            }
-            if (_.get(this.questionMetaData,'interactionTypes[0]') === 'choice') {
+            if (this.questionInteractionType === 'choice') {
               this.childFormData['allowMultiSelect'] = _.get(this.questionMetaData, 'responseDeclaration.response1.cardinality') === 'multiple' ? 'Yes' : 'No';
             }
             formFieldCategory.default = defaultValue;
