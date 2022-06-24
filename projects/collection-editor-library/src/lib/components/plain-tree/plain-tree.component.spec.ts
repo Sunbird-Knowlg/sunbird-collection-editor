8edde29a02a5645c459b8b7658f2338f891db990
pradeepkumarcm96 <pradeep.k@pacewisdom.com>
2022-06-24 14:50:25 +0530

    Issue #SB-27420 : test : test case fix

--- projects/collection-editor-library/src/lib/components/plain-tree/plain-tree.component.spec.ts
+++ projects/collection-editor-library/src/lib/components/plain-tree/plain-tree.component.spec.ts
@@ -1,49 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlainTreeComponent } from './plain-tree.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { treeData } from '../fancy-tree/fancy-tree.component.spec.data';
+import { editorConfig, treeData } from '../fancy-tree/fancy-tree.component.spec.data';
 import { collectionHierarchyMock } from '../question/question.component.spec.data';
 import { EditorService } from '../../services/editor/editor.service';
 import { mockTreedata } from './plain-tree.component.spec.data';
-
-const mockEditorService = {
-  editorConfig: {
-    config: {
-      renderTaxonomy: false,
-      hierarchy: {
-        level1: {
-          name: 'Module',
-          type: 'Unit',
-          mimeType: 'application/vnd.sunbird.questionset',
-          contentType: 'Observation',
-          iconClass: 'fa fa-folder-o',
-          children: {},
-        },
-      },
-    },
-  },
-  parentIdentifier: '',
-  optionsLength: 4,
-  selectedChildren: {
-    primaryCategory: 'Multiselect Multiple Choice Question',
-    label: 'Multiple Choice Question',
-    interactionType: 'choice',
-  },
-  getToolbarConfig: () => {},
-  getHierarchyObj: () => {},
-  fetchCollectionHierarchy: (questionSetId) => {
-    subscribe: (fn) => fn(collectionHierarchyMock);
-  },
-  updateCollection: (questionSetId, event) => {
-    subscribe: (fn) => fn({});
-  },
-  addResourceToQuestionset: (questionSetId, unitId, questionId) => {
-    subscribe: (fn) => fn({});
-  },
-  apiErrorHandling: () => {},
-  editorMode:'review',
-  submitRequestChanges :() =>{}
-};
 
 describe('PlainTreeComponent', () => {
   let component: PlainTreeComponent;
@@ -54,7 +15,7 @@ describe('PlainTreeComponent', () => {
       imports: [HttpClientTestingModule],
       declarations: [ PlainTreeComponent ],
       providers: [
-        { provide: EditorService, useValue: mockEditorService }
+        EditorService
       ],
     })
     .compileComponents();
@@ -64,6 +25,8 @@ describe('PlainTreeComponent', () => {
     fixture = TestBed.createComponent(PlainTreeComponent);
     editorService = TestBed.get(EditorService);
     component = fixture.componentInstance;
+    spyOnProperty(editorService, 'editorConfig', 'get').and.returnValue(editorConfig);
+    // component.ngAfterViewInit();
     // fixture.detectChanges();
   });
 
@@ -73,18 +36,23 @@ describe('PlainTreeComponent', () => {
 
   it('#buildTreeData should call', () => {
      spyOn(component, 'buildTreeData').and.callThrough();
-     editorService = mockEditorService;
      component.buildTreeData(mockTreedata);
      expect(component.buildTreeData).toHaveBeenCalledWith(mockTreedata);
   });
 
   it('#ngAfterViewInit() should call #getTreeConfig() and #renderTree()', () => {
     spyOn(component, 'ngAfterViewInit').and.callThrough();
-    editorService = mockEditorService;
     spyOn(component, 'renderTree');
     spyOn(component, 'getTreeConfig');
     component.ngAfterViewInit();
     expect(component.renderTree).toHaveBeenCalled();
     expect(component.getTreeConfig).toHaveBeenCalled();
+  });
+
+  it('#getQuestionsList should call', () => {
+    spyOn(component, 'getQuestionsList').and.callThrough();
+    spyOn(component.treeEmitter, 'emit');
+    component.getQuestionsList('do_123');
+    expect(component.treeEmitter.emit).toHaveBeenCalledWith({identifier : 'do_123'});
   });
 });
