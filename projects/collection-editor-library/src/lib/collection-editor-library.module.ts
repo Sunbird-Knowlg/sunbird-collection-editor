90c96ca0a982fd8118abb27ca4846df25a72fb5c
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2022-12-14 11:47:29 +0530

    Issue #KN-283 feat: Removed options component

--- projects/collection-editor-library/src/lib/collection-editor-library.module.ts
+++ projects/collection-editor-library/src/lib/collection-editor-library.module.ts
@@ -22,7 +22,6 @@ import { TemplateComponent } from './components/template/template.component';
 import { ResourceReorderComponent } from './components/resource-reorder/resource-reorder.component';
 import { SkeletonLoaderComponent } from './components/skeleton-loader/skeleton-loader.component';
 import { QumlplayerPageComponent } from './components/qumlplayer-page/qumlplayer-page.component';
-import { OptionsComponent } from './components/options/options.component';
 import { CkeditorToolComponent } from './components/ckeditor-tool/ckeditor-tool.component';
 import { QuestionComponent } from './components/question/question.component';
 import {SunbirdPdfPlayerModule} from '@project-sunbird/sunbird-pdf-player-v9';
@@ -72,7 +71,6 @@ import { QualityParamsModalComponent } from './components/quality-params-modal/q
     LibraryFilterComponent,
     LibraryListComponent,
     QuestionComponent,
-    OptionsComponent,
     CkeditorToolComponent,
     LibraryPlayerComponent,
     ResourceReorderComponent,
