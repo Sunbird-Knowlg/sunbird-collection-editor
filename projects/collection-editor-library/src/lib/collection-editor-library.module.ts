ca607bce27c55815aceb63e220e28586b420e6a6
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2022-12-14 11:46:01 +0530

    Issue #KN-283 feat: Removed answer component

--- projects/collection-editor-library/src/lib/collection-editor-library.module.ts
+++ projects/collection-editor-library/src/lib/collection-editor-library.module.ts
@@ -23,7 +23,6 @@ import { ResourceReorderComponent } from './components/resource-reorder/resource
 import { SkeletonLoaderComponent } from './components/skeleton-loader/skeleton-loader.component';
 import { QumlplayerPageComponent } from './components/qumlplayer-page/qumlplayer-page.component';
 import { OptionsComponent } from './components/options/options.component';
-import { AnswerComponent } from './components/answer/answer.component';
 import { CkeditorToolComponent } from './components/ckeditor-tool/ckeditor-tool.component';
 import { QuestionComponent } from './components/question/question.component';
 import {SunbirdPdfPlayerModule} from '@project-sunbird/sunbird-pdf-player-v9';
@@ -74,7 +73,6 @@ import { QualityParamsModalComponent } from './components/quality-params-modal/q
     LibraryListComponent,
     QuestionComponent,
     OptionsComponent,
-    AnswerComponent,
     CkeditorToolComponent,
     LibraryPlayerComponent,
     ResourceReorderComponent,
