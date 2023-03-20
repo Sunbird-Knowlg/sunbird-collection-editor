31cb33308802b9e809d99ca21de0653979e3b426
Harish Kumar Gangula <harish@sanketika.in>
2023-03-20 12:49:05 +0530

    Issue #KN-847 feat: Added web component

--- build-wc.js
+++ build-wc.js
@@ -1,5 +1,7 @@
 const fs = require("fs-extra");
 const concat = require("concat");
+const path = require("path")
+
 
 const build = async () => {
   const files = [
@@ -9,13 +11,22 @@ const build = async () => {
     "./dist/collection-ediotr-library-wc/main.js"
   ];
 
+  const filesToExclude = [
+    "dist/collection-ediotr-library-wc/index.html",
+    "dist/collection-ediotr-library-wc/runtime.js",
+    "dist/collection-ediotr-library-wc/polyfills.js",
+    "dist/collection-ediotr-library-wc/scripts.js",
+    "dist/collection-ediotr-library-wc/main.js"
+  ]
+  const filter = file => {
+     return !filesToExclude.includes(file);
+  }
+
   await fs.ensureDir("dist/collection-ediotr-library-wc");
   await concat(files, "web-component/sunbird-collection-editor.js");
-  await fs.copy("./dist/collection-ediotr-library-wc/assets", "web-component/assets");
-  await fs.copy("./dist/collection-ediotr-library-wc/styles.css", "web-component/styles.css")
+  await fs.copy("./dist/collection-ediotr-library-wc/", "web-component/", {filter});
   await concat(files, "web-component-demo/sunbird-collection-editor.js");
-  await fs.copy("./dist/collection-ediotr-library-wc/assets", "web-component-demo/assets");
-  await fs.copy("./dist/collection-ediotr-library-wc/styles.css", "web-component-demo/styles.css")
+  await fs.copy("./dist/collection-ediotr-library-wc/", "web-component-demo/", {filter});
   console.log("Files concatenated successfully!!!");
 };
 build();
