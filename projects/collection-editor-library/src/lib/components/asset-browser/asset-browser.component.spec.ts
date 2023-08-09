9765d4e0396575422f7f36a83490ed09974c32b3
Kartheek Palla <kartheek@sanketika.in>
2023-08-09 15:42:47 +0530

    Issue KN-902 issue: file upload library integrated

--- projects/collection-editor-library/src/lib/components/asset-browser/asset-browser.component.spec.ts
+++ projects/collection-editor-library/src/lib/components/asset-browser/asset-browser.component.spec.ts
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 import { EditorService } from '../../services/editor/editor.service';
 import { of, throwError } from 'rxjs';
 import * as _ from 'lodash-es';
-
+declare const SunbirdFileUploadLib: any;
 const mockEditorService = {
   editorConfig: {
     config: {
